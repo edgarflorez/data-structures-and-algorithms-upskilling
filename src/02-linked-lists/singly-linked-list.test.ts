b39fada43132c05eb2110c9ac0e0acd67739d410
Edgar Florez <edgarfmail@gmail.com>
2025-09-14 18:09:55 -0500

    Linked lists
    - Singly Linked List write a set of functions to practice.
    - insertAtFront
    - insertAtEnd
    - insertAt
    - deleteAtFront
    - deleteAtEnd
    - deleteAt
    - search
    - getAt

--- src/02-linked-lists/singly-linked-list.test.ts
+++ src/02-linked-lists/singly-linked-list.test.ts
@@ -1,7 +1,18 @@
-import LinkedList from "./singly-linked-list";
+import LinkedList from "./singly-linked-list.ts";
 
 describe("Linked List: Singly Linked List", () => {
-	it("returns true on printList", () => {
+	it("should return and empty array if no nodes added", () => {
+		const myLinkedList = new LinkedList();
+		const expected: number[] = [];
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected)
+	})
+})
+
+describe("Linked List: Singly Linked List, inserting", () => {
+	it("should return all the `numbers` inserted at the front", () => {
 		const myLinkedList = new LinkedList();
 		const expected = [ 3, 2, 1 ];
 		myLinkedList.insertAtFront(1)
@@ -11,6 +22,343 @@ describe("Linked List: Singly Linked List", () => {
 		const result = myLinkedList.printList();
 
 		expect(result).toStrictEqual(expected)
+	})
 
+	it("should return all the `strings` inserted at the front", () => {
+		const myLinkedList = new LinkedList();
+		const expected = [ "C", "B", "A" ];
+		myLinkedList.insertAtFront("A")
+		myLinkedList.insertAtFront("B")
+		myLinkedList.insertAtFront("C")
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected)
+	})
+
+	it("should return all the mixed values inserted at the front", () => {
+		const myLinkedList = new LinkedList();
+		const expected = [ 1, "B", "A" ];
+		myLinkedList.insertAtFront("A")
+		myLinkedList.insertAtFront("B")
+		myLinkedList.insertAtFront(1);
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected)
+	})
+
+	it('should return all the values inserted at the end', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [1,2,3]
+		myLinkedList.insertAtEnd(1)
+		myLinkedList.insertAtEnd(2)
+		myLinkedList.insertAtEnd(3)
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should return all the values after inserting at a given index, middle', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [1,2,3]
+		myLinkedList.insertAtEnd(1)
+		myLinkedList.insertAtEnd(3)
+		myLinkedList.insertAt(2, 1);
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should return all the values after inserting at a given index, front', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [1,2,3]
+		myLinkedList.insertAtEnd(2)
+		myLinkedList.insertAtEnd(3)
+		myLinkedList.insertAt(1, 0);
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should return all the values after inserting at a given index, end', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [1,2,3]
+		myLinkedList.insertAtEnd(1)
+		myLinkedList.insertAtEnd(2)
+		myLinkedList.insertAt(3, 2);
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should throw an error on inserting with a negative index', () => {
+		const myLinkedList = new LinkedList();
+		const expected = 'Error: Index out of bound'
+
+		const result = () => {
+			myLinkedList.insertAtEnd(1)
+			myLinkedList.insertAtEnd(2)
+			myLinkedList.insertAt(3, -1);
+		};
+
+		expect(result).toThrow(expected);
+	})
+
+	it('should throw an error on inserting -with a out of bound positive index', () => {
+		const myLinkedList = new LinkedList();
+		const expected = 'Error: Index out of bound'
+
+		const result = () => {
+			myLinkedList.insertAtEnd(1)
+			myLinkedList.insertAtEnd(2)
+			myLinkedList.insertAt(3, 10);
+		};
+
+		expect(result).toThrow(expected);
 	})
 })
+
+describe("Linked List: Singly Linked List, deleting", () => {
+	it('should delete the element in front', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [2,3];
+		myLinkedList.insertAtEnd(1)
+		myLinkedList.insertAtEnd(2)
+		myLinkedList.insertAtEnd(3)
+		myLinkedList.deleteAtFront()
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should delete two recently added elements in front', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [];
+		myLinkedList.insertAtEnd(1);
+		myLinkedList.insertAtEnd(2);
+		myLinkedList.deleteAtFront();
+		myLinkedList.deleteAtFront();
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should throw and error when try to delete at front on and empty Linked List', () => {
+		const myLinkedList = new LinkedList();
+		const expected = 'Error: is not possible delete on an empty Linked list';
+
+		const result = () => {
+			myLinkedList.insertAtEnd(1);
+			myLinkedList.deleteAtFront();
+			myLinkedList.deleteAtFront();
+		};
+
+		expect(result).toThrow(expected);
+	})
+
+	it('should delete the element at the end', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [1,2];
+		myLinkedList.insertAtEnd(1)
+		myLinkedList.insertAtEnd(2)
+		myLinkedList.insertAtEnd(3)
+		myLinkedList.deleteAtEnd()
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should delete two recently added elements at the end', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [];
+		myLinkedList.insertAtEnd(1);
+		myLinkedList.insertAtEnd(2);
+		myLinkedList.deleteAtEnd();
+		myLinkedList.deleteAtEnd();
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should throw and error when try to delete at front on and empty Linked List', () => {
+		const myLinkedList = new LinkedList();
+		const expected = 'Error: is not possible delete on an empty Linked list';
+
+		const result = () => {
+			myLinkedList.insertAtEnd(1);
+			myLinkedList.deleteAtEnd();
+			myLinkedList.deleteAtEnd();
+		};
+
+		expect(result).toThrow(expected);
+	})
+
+	it('should return a valid set of values after delete at a given index, middle', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [1,3]
+		myLinkedList.insertAtEnd(1)
+		myLinkedList.insertAtEnd(2)
+		myLinkedList.insertAtEnd(3)
+		myLinkedList.deleteAt(1);
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should return a valid set of values after inserting at a given index, front', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [2,3]
+		myLinkedList.insertAtEnd(1)
+		myLinkedList.insertAtEnd(2)
+		myLinkedList.insertAtEnd(3)
+		myLinkedList.deleteAt(0);
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should return a valid set of values after inserting at a given index, end', () => {
+		const myLinkedList = new LinkedList();
+		const expected = [1,2]
+		myLinkedList.insertAtEnd(1)
+		myLinkedList.insertAtEnd(2)
+		myLinkedList.insertAtEnd(3)
+		myLinkedList.deleteAt(2);
+
+		const result = myLinkedList.printList();
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should throw an error on trying to delete a negative index', () => {
+		const myLinkedList = new LinkedList();
+		const expected = 'Error: Index out of bound'
+
+		const result = () => {
+			myLinkedList.insertAtEnd(1)
+			myLinkedList.insertAtEnd(2)
+			myLinkedList.deleteAt(-1);
+		};
+
+		expect(result).toThrow(expected);
+	})
+
+	it('should throw an error on trying to insert a out of bound positive index', () => {
+		const myLinkedList = new LinkedList();
+		const expected = 'Error: Index out of bound'
+
+		const result = () => {
+			myLinkedList.insertAtEnd(1)
+			myLinkedList.insertAtEnd(2)
+			myLinkedList.deleteAt(2);
+		};
+
+		expect(result).toThrow(expected);
+	})
+})
+
+describe("Linked List: Singly Linked List, searching", () => {
+	it('should return true when searching for a value in the liked list', () => {
+		const myLinkedList = new LinkedList();
+		myLinkedList.insertAtFront(1)
+		myLinkedList.insertAtFront(2)
+		myLinkedList.insertAtFront(3)
+		const expected = true;
+
+		const result = myLinkedList.search(2);
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should return false when searching for a value not in the liked list', () => {
+		const myLinkedList = new LinkedList();
+		myLinkedList.insertAtFront(1)
+		myLinkedList.insertAtFront(2)
+		myLinkedList.insertAtFront(3)
+		const expected = false;
+
+		const result = myLinkedList.search(4);
+
+		expect(result).toStrictEqual(expected);
+	})
+
+	it('should return false when searching on an empty liked list', () => {
+		const myLinkedList = new LinkedList();
+		const expected = false;
+
+		const result = myLinkedList.search(1);
+
+		expect(result).toStrictEqual(expected);
+	})
+})
+
+describe("Linked List: Singly Linked List, getAt", () => {
+	it("should get the value of a given index", () => {
+		const myLinkedList = new LinkedList();
+		myLinkedList.insertAtFront("a")
+		myLinkedList.insertAtFront("b")
+		myLinkedList.insertAtFront("c")
+		const expected = "b";
+
+		const result = myLinkedList.getAt(1);
+
+		expect(result).toStrictEqual(expected);
+	})
+	it("should get undefined when trying to getAt on an empty Liked List", () => {
+		const myLinkedList = new LinkedList();
+		const expected = undefined;
+
+		const result = myLinkedList.getAt(1);
+
+		expect(result).toStrictEqual(expected);
+	})
+	it("should thrown an error for a index out of bound, upperbound", () => {
+		const myLinkedList = new LinkedList();
+		const expected = "Error: Index out of bound";
+
+		const result = () => {
+			myLinkedList.insertAtFront("a")
+			myLinkedList.insertAtFront("b")
+			myLinkedList.insertAtFront("c")
+			myLinkedList.getAt(3)
+		};
+
+		expect(result).toThrow(expected);
+	})
+	it("should thrown an error for a index out of bound, lowerbound", () => {
+		const myLinkedList = new LinkedList();
+		const expected = "Error: Index out of bound";
+
+		const result = () => {
+			myLinkedList.insertAtFront("a")
+			myLinkedList.insertAtFront("b")
+			myLinkedList.insertAtFront("c")
+			myLinkedList.getAt(-1)
+		};
+
+		expect(result).toThrow(expected);
+	})
+})
+
+
+// const myLinkedList = new LinkedList();
+// 		const expected = 'Error: Index out of bound'
+
+// 		const result = () => {
+// 			myLinkedList.insertAtEnd(1)
+// 			myLinkedList.insertAtEnd(2)
+// 			myLinkedList.insertAt(3, 10);
+// 		};
+
+// 		expect(result).toThrow(expected);
--- src/02-linked-lists/singly-linked-list.ts
+++ src/02-linked-lists/singly-linked-list.ts
@@ -1,3 +1,22 @@
+/*
+Implement a class LinkedList in TypeScript that represents a singly linked list. Each node should store a value and a reference to the next node.
+
+Your linked list should support the following methods:
+
+- insertAtFront(value) – Insert a new node at the beginning of the list.
+- insertAtEnd(value) – Insert a new node at the end of the list.
+- insertAt(index, value) – Insert a new node at a specific position.
+- deleteAtFront() – Remove the first node.
+- deleteAtEnd() – Remove the last node.
+- deleteAt(index) – Remove a node at a specific position.
+- search(value) – Return true if the value exists in the list, otherwise false.
+- getAt(index) – Return the value at a specific position.
+- size() – Return the total number of nodes in the list.
+- isEmpty() – Return true if the list is empty.
+- printList() – Print all values in the list.
+- reverse() – Reverse the linked list in place.
+ */
+
 class Node<T> {
 	data: T;
 	next: Node<T> | null;
@@ -8,36 +27,218 @@ class Node<T> {
 	}
 }
 
-export default class LinkedList<J> {
-	head: Node<J> | null;
+export default class LinkedList<T> {
+	head: Node<T> | null;
+	tail: Node<T> | null;
+	size = 0;
 
 	constructor() {
 		this.head = null;
+		this.tail = null;
 	}
 
-	insertAtFront = (x:J) => {
-		let newNode = new Node(x);
+	private resetLinkedList() {
+		this.head = this.tail = null;
+	}
+
+	insertAtFront = (x:T) => {
+		const newNode = new Node(x);
 		if(!this.head) {
-			this.head = newNode
+			this.head = newNode;
+			this.tail = newNode;
+			this.size = 1;
 			return;
 		}
 
 		newNode.next = this.head;
 		this.head = newNode;
+		this.size++;
+	}
+
+	insertAtEnd = (x:T) => {
+		const newNode = new Node(x);
+		if(!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+			this.size = 1;
+			return;
+		}
+		if(this.tail) {
+			this.tail.next = newNode;
+		}
+		this.tail = newNode;
+		this.size++;
+	}
+
+	/**
+	 *
+	 * @param x new value
+	 * @param insertIndex position for the new value, the index should be within bounds `0 <= index <= size`
+	 * @returns
+	 */
+	insertAt = (x:T, insertIndex: number) => {
+		if(insertIndex < 0 || insertIndex > this.size + 1) {
+			throw new Error("Error: Index out of bound")
+		}
+
+		if(insertIndex === 0) {
+			this.insertAtFront(x);
+			return
+		}
+
+		if(insertIndex === this.size){
+			this.insertAtEnd(x);
+			return;
+		}
+
+		if(this.head) {
+			const newNode = new Node(x);
+			let counter = 0;
+			let pointer: Node<T>  = this.head;
+			while (counter <= insertIndex) {
+				// The modification should be done on the previous node.
+				if(counter + 1 === insertIndex) {
+					newNode.next = pointer.next;
+					pointer.next = newNode;
+					break;
+				}
+				if(pointer.next) {
+					pointer = pointer.next;
+				}
+				counter++
+			}
+		}
+	}
+
+	deleteAtFront = () => {
+		if(!this.size){
+			throw new Error('Error: is not possible delete on an empty Linked list')
+		}
+
+		if(this.head) {
+			const newHead = this.head.next;
+			this.head = newHead;
+			this.size--;
+		}
+
+		if(!this.size) {
+			this.resetLinkedList();
+		}
+	}
+
+	deleteAtEnd = () => {
+		if(!this.size){
+			throw new Error('Error: is not possible delete on an empty Linked list')
+		}
+
+		if(this.size === 1) {
+			this.deleteAtFront();
+		}
+
+		if(this.head) {
+			let counter = 0;
+			let pointer: Node<T>  = this.head;
+			const previousNodeIndex = this.size - 2;
+
+			while (counter <= previousNodeIndex) {
+				if(counter === previousNodeIndex) {
+					pointer.next = null;
+					this.size--;
+					break;
+				}
+				if(pointer.next) {
+					pointer = pointer.next;
+				}
+				counter++
+			}
+		}
+
+		if(this.size === 0) {
+			this.resetLinkedList();
+		}
+	}
+
+	deleteAt = (deleteIndex: number) => {
+		if(deleteIndex < 0 || deleteIndex >= this.size) {
+			throw new Error("Error: Index out of bound");
+		}
+
+		if(deleteIndex === 0) {
+			this.deleteAtFront();
+		}
+
+		if(this.head) {
+			let counter = 0;
+			let pointer: Node<T> = this.head;
+			while(counter <= deleteIndex) {
+				// The modification should be done on the previous node
+				if(counter + 1 === deleteIndex) {
+					pointer.next = pointer.next?.next || null;
+					break;
+				}
+				if(pointer.next) {
+					pointer = pointer.next;
+				}
+				counter++
+			}
+		}
+	}
+
+	search = (query: T) => {
+		if(!this.size) {
+			return false;
+		}
+
+		if(this.head) {
+			let counter = 0;
+			let pointer: Node<T> = this.head;
+			while(counter < this.size) {
+				if(pointer.data === query) {
+					return true;
+				}
+				if(pointer.next) {
+					pointer = pointer.next;
+				}
+				counter++
+			}
+
+			return false
+		}
+	}
+
+	getAt = (getIndex:number) => {
+		if(!this.size) {
+			return undefined;
+		}
+
+		if(getIndex < 0 || getIndex >= this.size) {
+			throw new Error("Error: Index out of bound")
+		}
+
+		if(this.head) {
+			let counter = 0;
+			let pointer: Node<T> = this.head;
+			while(counter < this.size) {
+				if(counter === getIndex) {
+					return pointer.data;
+				}
+				if(pointer.next) {
+					pointer = pointer.next;
+				}
+				counter++
+			}
+		}
 	}
 
 	printList = () => {
-		let response: J[] = []
+		let response: T[] = []
 
 		let curr:Node = this.head;
 		while (curr !== null) {
-			// process.stdout.write(curr.data.toString());
 			response.push(curr.data);
-			// if (curr.next !== null) {
-			// 	process.stdout.write(" -> ");
-			// }
 			curr = curr.next;
 		}
+
 		return response;
 	}
 }
